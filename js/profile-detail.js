4921f4383d15c695825143aee97b93e43a6a8edf
temirovv <temirovv21@gmail.com>
2024-08-11 17:38:49 +0500

    url apis updated

--- js/profile-detail.js
+++ js/profile-detail.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('product_id');
-    const apiUrl = `http://77.232.131.69/api/product-detail/${productId}`;  // Adjust the URL to fetch the specific product
+    const apiUrl = `https://77.232.131.69/api/product-detail/${productId}`;  // Adjust the URL to fetch the specific product
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             // Update the page with the product details
-            document.getElementById('product-image').src = 'http://localhost:8000/' +  data.image[0].image;
+            document.getElementById('product-image').src = 'https://77.232.131.69/' +  data.image[0].image;
             document.getElementById('product-title').textContent = data.name;
             document.getElementById('current-price').textContent = `${data.price} so'm`;
 
